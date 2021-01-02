13662a56bfde2880088df308fe2cb9da22b7229e
Lalit Sharma <lalitkumar2k10>
2021-01-02 09:15:40 +0530

    added sale order line pop up

---
+++ __manifest__.py
@@ -0,0 +1,18 @@
+{
+    "name": "Sale Edit",
+    "category": 'Uncategorized',
+    "summary": """Sale edit""",
+    "description": """Sale edit""",
+    "sequence": 1,
+    "author": "dev",
+    "website": "https://www.dev.com",
+    "version": '1.0',
+    "depends": ['sale'],
+    "data": [
+        'views/product_product.xml'
+    ],
+
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
